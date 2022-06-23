f8bef40c65f49b77b94813f05102e639ab7fe101
Zakaria Hamdani <105652085+Zakaria-hmd@users.noreply.github.com>
2022-06-23 11:04:34 +0200

    exo objet

---
+++ exo Dom/script.js
@@ -0,0 +1,25 @@
+//let array1 = [];
+ //onsole.log(array1.length);
+ 
+//var prénom = ["Zakaria","nico","Laura","Sandra","iles"]
+    //prénom.push;
+    //console.log(prénom)
+
+   
+
+    //const prénom = ["Zakaria","nico","Laura","Sandra","iles"]
+    //prénom.sort();
+    //console.log(prénom);
+    //prénom.reverse();
+  
+    function f(){
+            setTimeout("f()",1000);
+            d=new date();
+            h=d.getHours();
+            m=d.getMinutes();
+            s=d.getSeconds();
+            document.fo.horloge.value=h+" "+m+" "+s;
+                
+
+    }
+    
