1bc2b7bfebaf411c14b10bed6c7cccb8c5ad70b8
Zakaria Hamdani <105652085+Zakaria-hmd@users.noreply.github.com>
2022-06-24 11:02:57 +0200

    nombre premier

---
+++ exo nbr entier/script.js
@@ -0,0 +1,11 @@
+
+
+
+//function nbrPremier(nbr) {
+            //for (var i = 2; i < nbr; i++)
+              //  if (nbr % i === 0) return false;
+            //return nbr > 1;
+           
+        //}
+        alert(nbrPremier(1));
+        document.getElementById("first")
