4a0941fa31233367cc6b83d4bc46a384db5aa2bd
Zakaria Hamdani <105652085+Zakaria-hmd@users.noreply.github.com>
2022-07-18 09:31:10 +0200

    exo dom

--- exo_Formulaire/script.js
+++ exo_Formulaire/script.js
@@ -1,9 +1,35 @@
-//function checkForm(f) {
-    //alert("je m'appelle Zakaria Hamdani , 25 ans " + f.elements['chp'].value); returnfalse; // n’envoie pas le formulaire     
-//}
-function getValue() {
-    // Sélectionner l'élément input et récupérer sa valeur
-    var input = document.getElementById("in").value;
-    // Afficher la valeur
-    alert(input);
+//recup des élements 
+const form = document.querySelector('#form');
+const username = document.querySelector('#username');
+const email = document.querySelector('#email');
+const password = document.querySelector('#password');
+const password2 = document.querySelector('#password2');
+
+// evenemtns
+form.addEventListener('clik',e=>{
+    e.preventDefault();
+    form_verify();
+})
+
+
+//Fonctions
+function form_verify(){
+// Obtenir toutes les valeurs des inputs
+const userValue = username.value.trim();
+const emailValue = email.value.trim();
+const passwordValue = password.value.trim();
+const password2Value = password2.value.trim();
+// username verify
+if (userValue ===""){
+    let mesage = "username ne peut pas être vide"; 
+    setError(username,message)
+}
+}
+function setError(elem,message){
+    const formControl = elem.parentElement;
+    const small = formControl.querySelector('small');
+    // ajout du message d'erreur
+    small.innertText = message
+    // ajout de la classe error 
+    formControl.className = "form-control error";
 }
